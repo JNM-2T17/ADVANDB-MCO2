912fba859f2176277be5704e9603a478b1b4bbb7
Austin Fernandez <ryanaustinf@yahoo.com>
2016-03-15 01:32:10 +0800

    Actual Fields

--- WebContent/resources/assets/js/script.js
+++ WebContent/resources/assets/js/script.js
@@ -6,79 +6,372 @@ $(document).ready(function(){
 	var dimensions = {
 		crops : 		[
 							{
-								name   : "crops1",
+								name   : "tenur",
+								display : "Owner Tenure",
 								range  : false,
-								values : ["House", "Cave", "Boat"]
+								values : [
+									"Owner, owner-like possession of house and lot",
+									"Rent house/room including lot",
+									"Own house, rent lot",
+									"Own house, rent-free lot with consent of owner",
+									"Own house, rent-free lot without consent of owner",
+									"Rent-free house and lot with consent of owner",
+									"Rent-free house and lot without consent of owner",
+									"Living in a public space with rent",
+									"Living in a public space without rent",
+									"Other tenure status"
+								]
 						  	},
 						  	{
-								name   : "crops2",
-								range  : true,
-								values : ["Grade 1", "Grade 2", "Grade 3", "Grade 4", "Grade 5"]
+								name   : "wall",
+								display : "Wall Type",
+								range  : false,
+								values : ["Strong materials (tile,concrete, brick, stone, wood, plywood)",
+								          "Light materials (bamboo, sawali, cogon, nipa, anahaw)",
+								          "Salvaged/makeshift materials",
+								          "Mixed but predominantly strong materials",
+								          "Mixed but predominantly light materials",
+								          "Mixed but predominantly salvaged materials",
+								          "Not applicable"]
 						  	},
 						  	{
-								name   : "crops3",
-								range  : true,
-								values : ["Cookies", "Cake", "Ice Cream", "Brownies"]
+						  		name   : "roof",
+								display : "Roof Type",
+								range  : false,
+								values : ["Strong materials (tile,concrete, brick, stone, wood, plywood)",
+								          "Light materials (bamboo, sawali, cogon, nipa, anahaw)",
+								          "Salvaged/makeshift materials",
+								          "Mixed but predominantly strong materials",
+								          "Mixed but predominantly light materials",
+								          "Mixed but predominantly salvaged materials",
+								          "Not applicable"]
+						  	},
+						  	{
+						  		name   : "house_type",
+								display : "House Type",
+								range  : false,
+								values : ["Single house",
+								          "Duplex",
+								          "Multi-unit residential (three units or more)",
+								          "Commercial/industrial/agricultural building/house",
+								          "Others (boat, cave, etc.)"]
+						  	},
+						  	{
+						  		name   : "location",
+								display : "Location",
+								range  : false,
+								values : []
+						  	},
+						  	{
+						  		name   : "water",
+								display : "Water Source",
+								range  : false,
+								values : ["Own faucet, community water system",
+								          "Shared faucet, community water system",
+								          "Own use tubed/piped deep well",
+								          "Shared tubed/piped deep well",
+								          "tubed/piped shallow well",
+								          "Dug well",
+								          "Protected Spring",
+								          "Unprotected Spring",
+								          "Lake, river, rain and others",
+								          "Peddler",
+								          "Bottled water",
+								          "Others"]
+						  	},
+						  	{
+						  		name   : "croptype",
+								display : "Crop Type",
+								range  : false,
+								values : ["Sugar cane",
+								          "Palay",
+								          "Corn",
+								          "Coffee",
+								          "Other Crops"]
 						  	}
 			   			],
 		landParcel : 	[
 							{
-								name   : "land1",
+								name   : "tenur",
+								display : "Owner Tenure",
 								range  : false,
-								values : ["House", "Cave", "Boat"]
+								values : [
+									"Owner, owner-like possession of house and lot",
+									"Rent house/room including lot",
+									"Own house, rent lot",
+									"Own house, rent-free lot with consent of owner",
+									"Own house, rent-free lot without consent of owner",
+									"Rent-free house and lot with consent of owner",
+									"Rent-free house and lot without consent of owner",
+									"Living in a public space with rent",
+									"Living in a public space without rent",
+									"Other tenure status"
+								]
 						  	},
 						  	{
-								name   : "land2",
-								range  : true,
-								values : ["Grade 1", "Grade 2", "Grade 3", "Grade 4", "Grade 5"]
-						  	},
-						  	{
-								name   : "land3",
+								name   : "wall",
+								display : "Wall Type",
 								range  : false,
-								values : ["Cookies", "Cake", "Ice Cream", "Brownies"]
+								values : ["Strong materials (tile,concrete, brick, stone, wood, plywood)",
+								          "Light materials (bamboo, sawali, cogon, nipa, anahaw)",
+								          "Salvaged/makeshift materials",
+								          "Mixed but predominantly strong materials",
+								          "Mixed but predominantly light materials",
+								          "Mixed but predominantly salvaged materials",
+								          "Not applicable"]
 						  	},
 						  	{
-								name   : "land4",
-								range  : true,
-								values : ["0.0", "1.0", "1.5", "2.0", "2.5", "3.0", "3.5", "4.0"]
+						  		name   : "roof",
+								display : "Roof Type",
+								range  : false,
+								values : ["Strong materials (tile,concrete, brick, stone, wood, plywood)",
+								          "Light materials (bamboo, sawali, cogon, nipa, anahaw)",
+								          "Salvaged/makeshift materials",
+								          "Mixed but predominantly strong materials",
+								          "Mixed but predominantly light materials",
+								          "Mixed but predominantly salvaged materials",
+								          "Not applicable"]
+						  	},
+						  	{
+						  		name   : "house_type",
+								display : "House Type",
+								range  : false,
+								values : ["Single house",
+								          "Duplex",
+								          "Multi-unit residential (three units or more)",
+								          "Commercial/industrial/agricultural building/house",
+								          "Others (boat, cave, etc.)"]
+						  	},
+						  	{
+						  		name   : "location",
+								display : "Location",
+								range  : false,
+								values : []
+						  	},
+						  	{
+						  		name   : "water",
+								display : "Water Source",
+								range  : false,
+								values : ["Own faucet, community water system",
+								          "Shared faucet, community water system",
+								          "Own use tubed/piped deep well",
+								          "Shared tubed/piped deep well",
+								          "tubed/piped shallow well",
+								          "Dug well",
+								          "Protected Spring",
+								          "Unprotected Spring",
+								          "Lake, river, rain and others",
+								          "Peddler",
+								          "Bottled water",
+								          "Others"]
 						  	}
 					   	],
 		arcdp : 		[
 							{
-								name   : "arcdp1",
+								name   : "tenur",
+								display : "Owner Tenure",
 								range  : false,
-								values : ["House", "Cave", "Boat"]
+								values : [
+									"Owner, owner-like possession of house and lot",
+									"Rent house/room including lot",
+									"Own house, rent lot",
+									"Own house, rent-free lot with consent of owner",
+									"Own house, rent-free lot without consent of owner",
+									"Rent-free house and lot with consent of owner",
+									"Rent-free house and lot without consent of owner",
+									"Living in a public space with rent",
+									"Living in a public space without rent",
+									"Other tenure status"
+								]
 						  	},
 						  	{
-								name   : "arcdp2",
-								range  : true,
-								values : ["Grade 1", "Grade 2", "Grade 3", "Grade 4", "Grade 5"]
+								name   : "wall",
+								display : "Wall Type",
+								range  : false,
+								values : ["Strong materials (tile,concrete, brick, stone, wood, plywood)",
+								          "Light materials (bamboo, sawali, cogon, nipa, anahaw)",
+								          "Salvaged/makeshift materials",
+								          "Mixed but predominantly strong materials",
+								          "Mixed but predominantly light materials",
+								          "Mixed but predominantly salvaged materials",
+								          "Not applicable"]
 						  	},
 						  	{
-								name   : "arcdp3",
+						  		name   : "roof",
+								display : "Roof Type",
 								range  : false,
-								values : ["Cookies", "Cake", "Ice Cream", "Brownies"]
+								values : ["Strong materials (tile,concrete, brick, stone, wood, plywood)",
+								          "Light materials (bamboo, sawali, cogon, nipa, anahaw)",
+								          "Salvaged/makeshift materials",
+								          "Mixed but predominantly strong materials",
+								          "Mixed but predominantly light materials",
+								          "Mixed but predominantly salvaged materials",
+								          "Not applicable"]
+						  	},
+						  	{
+						  		name   : "house_type",
+								display : "House Type",
+								range  : false,
+								values : ["Single house",
+								          "Duplex",
+								          "Multi-unit residential (three units or more)",
+								          "Commercial/industrial/agricultural building/house",
+								          "Others (boat, cave, etc.)"]
+						  	},
+						  	{
+						  		name   : "location",
+								display : "Location",
+								range  : false,
+								values : []
+						  	},
+						  	{
+						  		name   : "water",
+								display : "Water Source",
+								range  : false,
+								values : ["Own faucet, community water system",
+								          "Shared faucet, community water system",
+								          "Own use tubed/piped deep well",
+								          "Shared tubed/piped deep well",
+								          "tubed/piped shallow well",
+								          "Dug well",
+								          "Protected Spring",
+								          "Unprotected Spring",
+								          "Lake, river, rain and others",
+								          "Peddler",
+								          "Bottled water",
+								          "Others"]
+						  	},
+						  	{
+						  		name : "sch_type",
+								display : "School Type",
+						  		range : false,
+						  		values : ["Public","Private"]
+						  	},
+						  	{
+						  		name : "workcl",
+								display : "Work Class",
+						  		range : false,
+						  		values : ["Working for private household",
+						  		        "Working for private business/establishment/farm",
+						  		      "Working for government/corporation",
+						  		      "Self-employed without any employee",
+						  		      "Employer in own family-operated or business",
+						  		      "Working with pay on family-operated or business",
+						  		      "Working without pay on family-operated or business"]
+						  	},
+						  	{
+						  		name : "jobind",
+								display : "Job Index",
+						  		range : false,
+						  		values : ["Employed","Unemployed"]
+						  	},
+						  	{
+						  		name : "jstatus",
+								display : "Job Status",
+						  		range : false,
+						  		values : ["Permanent",
+						  		        "Short-term, seasonal or casual",
+						  		      "Worked on different jobs on day to day or week to week"]
+						  	},
+						  	{
+						  		name : "gradel",
+								display : "Current Grade Level",
+						  		range : true,
+						  		values : ["Day Care",
+						  		        "Nursery/Kindergarten/Preparatory",
+						  		      "Grade 1",
+						  		      "Grade 2",
+						  		      "Grade 3",
+						  		      "Grade 4",
+						  		      "Grade 5",
+						  		      "Grade 6",
+						  		      "Grade 7",
+						  		      "Grade 8",
+						  		      "Grade 9/3rd Year HS",
+						  		      "Grade 10/4th Year HS",
+						  		      "Grade 11",
+						  		      "Grade 12",
+						  		      "1st year PS PS/N-T/TV",
+						  		      "2nd year PS PS/N-T/TV",
+						  		      "3rd year PS PS/N-T/TV",
+						  		      "1st year College",
+						  		      "2nd year College",
+						  		      "3rd year College",
+						  		      "4th year College or higher",
+						  		      "Post grad with units",
+						  		      "ALS Elementary",
+						  		      "ALS Secondary",
+						  		      "SPED Elementary",
+						  		      "SPED Secondary"]
+						  	},
+						  	{
+						  		name : "educal",
+								display : "Highest Educational Attainment",
+						  		range : true,
+						  		values : ["No Grade",
+						  		          "Day Care",
+						  		        "Nursery/Kindergarten/Preparatory",
+						  		      "Grade 1",
+						  		      "Grade 2",
+						  		      "Grade 3",
+						  		      "Grade 4",
+						  		      "Grade 5",
+						  		      "Grade 6",
+						  		      "Grade 7",
+						  		      "Grade 8",
+						  		      "Grade 9/3rd Year HS",
+						  		      "Grade 10/4th Year HS",
+						  		      "Grade 11",
+						  		      "Grade 12",
+						  		      "1st year PS PS/N-T/TV",
+						  		      "2nd year PS PS/N-T/TV",
+						  		      "3rd year PS PS/N-T/TV",
+						  		      "1st year College",
+						  		      "2nd year College",
+						  		      "3rd year College",
+						  		      "4th year College or higher",
+						  		      "Post grad with units",
+						  		      "ALS Elementary",
+						  		      "ALS Secondary",
+						  		      "SPED Elementary",
+						  		      "SPED Secondary",
+						  		      "Grade school graduate",
+						  		      "High school graduate",
+						  		      "Post secondary graduate",
+						  		      "College graduate",
+						  		      "Master's/PhD graduate"]
 						  	}
 					   ]
 	};
+	
+	for( x in dimensions ) {
+		for( y in dimensions[x] ) {
+			if( dimensions[x][y].name === "location ") {
+				for( i = 1; i <= 3078; i++ ) {
+					dimensions[x][y].values.push(i);
+				}
+			}
+		}
+	}
+	
+	var active;
 
 	setupFactTable(dimensions.crops);
 
 	$("#navFactTable button").click(function(){
 		$("#navFactTable button").removeClass("active");
 		$(this).addClass("active");
-		var currTable;
 		switch($(this).attr("data-factTable")){
 			case "crops":
-				currTable = dimensions.crops;
+				active = dimensions.crops;
 				break;
 			case "landParcel":
-				currTable = dimensions.landParcel;
+				active = dimensions.landParcel;
 				break;
 			case "ARCDP":
-				currTable = dimensions.arcdp;
+				active = dimensions.arcdp;
 		}
-		setupFactTable(currTable);
+		setupFactTable(active);
 	});
 
 	$("#buttonApply").click(function(){
@@ -138,7 +431,7 @@ function setupFactTable(currTable){
 
 	var string = "";
 	for(var i = 0; i < currTable.length; i++){
-		string += "<input class=\"checkRollDrill\" type=\"checkbox\" value=\"" + currTable[i].name + "\"> " + currTable[i].name + "<br>\n";
+		string += "<input class=\"checkRollDrill\" type=\"checkbox\" value=\"" + currTable[i].name + "\"> " + currTable[i].display + "<br>\n";
 	}
 	$("#optionsRD").html(string);
 
@@ -146,7 +439,7 @@ function setupFactTable(currTable){
 	for(var i = 0; i < currTable.length; i++){
 		string = "<tr data-column=\"" + currTable[i].name + "\">\n" 
 					+ "<td><input class=\"checkSliceDice\" type=\"checkbox\"></td>\n"
-					+ "<td>" + currTable[i].name + "</td>\n"
+					+ "<td>" + currTable[i].display + "</td>\n"
 					+ "<td class=\"choices\">\n</td>\n</tr>\n";
 
 		$("#tableSliceDice").append(string);
@@ -167,7 +460,50 @@ function setupFactTable(currTable){
 		}
 
 		for(var j = 0; j < currTable[i].values.length; j++){
-			$(selectRow + " select").append("<option value=\"" + currTable[i].values[j] + "\">" + currTable[i].values[j] + "</option>\n");
+			var value;
+			if(currTable[i].name == "educal" ) {
+				if( j < 3 ) {
+					value = j;
+				} else if( j < 18 ){
+					value = j + 8;
+				} else if( j < 22 ){
+					value = j + 13;
+				} else if( j < 23 ) {
+					value = j + 19;
+				} else if( j < 27 ) {
+					value = j + 28;
+				} else switch ( j ) {
+					case 27:
+						value = 100;
+						break;
+					case 28:
+						value = 200;
+						break;
+					case 29:
+						value = 210;
+						break;
+					case 30:
+						value = 300;
+						break;
+					default:
+						value = 400;
+				}
+			} else if(currTable[i].name == "gradel" ) {
+				if( j < 2 ) {
+					value = j + 1;
+				} else if( j < 17 ){
+					value = j + 9;
+				} else if( j < 21 ){
+					value = j + 14	;
+				} else if( j < 22 ) {
+					value = j + 20;
+				} else {
+					value = j + 29;
+				}
+			} else {
+				value = j + 1;
+			}
+			$(selectRow + " select").append("<option value=\"" + value + "\">" + currTable[i].values[j] + "</option>\n");
 		}
 	}
 
@@ -200,7 +536,7 @@ function addListeners(){
 			$(this).parent().parent().find(".inputSliceDice").prop( "disabled", true );
 		}
 	});
-
+	
 	$(".inputRange:last-of-type").change(function(){
 		var chosenIndex = $(this).find("option:selected").index();
 		var firstDropDown = $(this).parent().find(".inputRange:first-of-type");
